83dde9fa4a1d1f94ba01da3e35a9534bf62fb295
kirankolasani <kirankumark906@gmail.com>
2023-08-17 20:35:56 +0530

    updated scripts

--- 06-array.sh
+++ 06-array.sh
@@ -2,6 +2,6 @@
 
 PERSONS=("kiran" "Ravi" "Madhu") #Array
 
-echo "First persion is : $PERSONS[0]"
-echo "All PERSONS are : $@"
-echo "Num of PERSONS are : $#"
+echo "First persion is : ${PERSONS[0]}"
+echo "All PERSONS are : ${PERSONS[@]}"
+echo "Num of PERSONS are : ${PERSONS[#]}"
