7b15a6c9773e2145fe3fcfcd5ec1b91022cfb94c
kirankolasani <kirankumark906@gmail.com>
2023-08-17 20:22:12 +0530

    updated scripts

--- 05-variable.sh
+++ 05-variable.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+R=\e[31m
+G=\e[32m
+N=\e[0m
+
 echo "Please enter user name : "
-read user
+read -s user
 
 echo "Plz enter password : "
-read pwd
+read -s pwd
 
-echo "Ur username is : $user"
+echo "Ur username is : $G $user $N"
 
-echo "Ur password is : $pwd"
+echo "Ur password is : $R $pwd $N"
