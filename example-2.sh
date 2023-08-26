fc750b84de06556cb0a391b527254d1561757e22
kirankolasani <kirankumark906@gmail.com>
2023-08-26 19:38:06 +0530

    example

--- example-2.sh
+++ example-2.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 Validate() {
-    read -p "$1" P1
-    if [ -z "$P1" ]; then
-        echo "It's empty. Please enter a name."
-    elif [[ ! "$P1" =~ ^[a-zA-Z]+$ ]]; then
-        echo "Please enter a valid string value, using only letters."
-    else
-        break
-    fi
+    while true; do
+        read -p "$1" P1
+        if [ -z "$P1" ]; then
+            echo "It's empty. Please enter a name."
+        elif [[ ! "$P1" =~ ^[a-zA-Z]+$ ]]; then
+            echo "Please enter a valid string value, using only letters."
+        else
+            break
+        fi
+    done
 }
 
-while true; do
-    Validate "Please enter 1st Person name: "
-done
+Validate "Please enter 1st Person name: "
 
 echo "Hey $P1, how are you?"
