409a9fdd606e51496a756b01d7f2fabbc53722cf
kirankolasani <kirankumark906@gmail.com>
2023-08-26 18:43:56 +0530

    example

--- example-1.sh
+++ example-1.sh
@@ -14,13 +14,6 @@ echo "Time is now : $Date"
 # P1=$1
 # P2=$2
 
-# Reading inputs from command propmt
-
-echo "Please provide 1st Person Name : "
-read P1
-echo "Please provide 2nd user name : "
-read -s P2
-
 # if [ $# != 2 ]
 # then 
 #   echo "Plz pass 2 Args at command line"
@@ -28,12 +21,26 @@ read -s P2
 #   exit
 # fi
 
-if [ $P1 == NULL ]
-then 
-  echo "Plz provide name, It's not empty"
-  read P1
-  exit
-fi
+# Reading inputs from command propmt
+
+# echo "Please provide 1st Person Name : "
+# read P1
+# echo "Please provide 2nd user name : "
+# read -s P2 
+
+## checking user enter input is not empty.
+
+read -p "Plz enter 1st person name: " P1
+ while true
+ do 
+   if [ -n "$P1" ]
+   then
+     break # exit the loop if user enter thre value
+   else 
+     echo "It's not empty, Plz enter name again "   
+   fi
+
+ done
 
 echo "Hey $P1, H R U ?"
 echo "Heloo $P2, I'm good what about u ?"
