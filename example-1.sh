2de5157d7d2f6046728eef99e49394ab52cfce89
kirankolasani <kirankumark906@gmail.com>
2023-08-26 17:38:53 +0530

    example

--- example-1.sh
+++ example-1.sh
@@ -5,3 +5,10 @@ echo "Hello , KIran welcome to Shell scripting"
 Date=$(date)
 
 echo "Time is now : $Date"
+
+P1=Kiran
+P2=Ram
+
+echo "Hey $P1, H R U ?"
+echo "Heloo $P2, I'm good what about u ?"
+echo "I'm alos too good $P1"
