84db1d20c90a47ab7d84a561cc80c9b14ece3b0d
kirankolasani <kirankumark906@gmail.com>
2023-08-27 10:49:27 +0530

    practice

--- mysql-install.sh
+++ mysql-install.sh
@@ -3,6 +3,7 @@
 # This script is to install mysql 
 Logdir="`pwd`/../logs"
 logfile=$Logdir/$0-$(date +%F).log
+>$logfile
 user=$(id -u)
 Validate() {
     if [ $1 == 0 ]
@@ -14,13 +15,13 @@ Validate() {
 }
 if [ $user == 0 ]
 then 
-   echo "this script is running under the user `id`" &>>$logfile
-   yum insatll mysql -y &>>$logfile
+   echo "this script is running under the user `id`" 
+   yum insatll mysql -y 
 else
-  echo "This mysql installation can't be run with normal user, so running with root user" &>>$logfile
-  sudo yum install mysql -y &>>$logfile
+  echo "This mysql installation can't be run with normal user, so running with root user" 
+  sudo yum install mysql -y 
   Validate $? "mysql installed -- successfully"
 fi  
 
-echo "mysql is installed successfully" &>>$logfile
+#echo "mysql is installed successfully" &>>$logfile
 echo $logfile
