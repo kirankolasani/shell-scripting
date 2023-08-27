487d89ad4833c25cbe8db7c4558e71c2b1fcf586
kirankolasani <kirankumark906@gmail.com>
2023-08-27 16:03:20 +0530

    practice

--- disk-usage.sh
+++ disk-usage.sh
@@ -2,7 +2,7 @@
 
 disk_usage_list=$(df -hT)
 threashold_value="10%"
-message=""
+message="$0.log"
 while IFS= read -r line
 do 
 #   echo "Reading line : $line"
@@ -10,16 +10,13 @@ do
   if [ -n "$usage" ]; then
 
     usage_percentage="${usage%\%}"  # Remove '%' from the usage value
-
+    
     if [ "$usage_percentage" -ge "${threashold_value%\%}" ]
     then
-    #   echo "$usage is greather $threashold_value"
-      message+="$usage is greather $threashold_value"
-    #   echo "$(df -hT|grep "Use%")"
-      message+= "$(df -hT|grep "Use%")"
-    #   echo "$line"
-      message+="$line"
-      echo "$message"
+      echo "$usage is greather $threashold_value" &>>$message
+      echo "$(df -hT|grep "Use%")" &>>$message
+      echo "$line" &>>$message
+      echo "$message" | mail -s "High Disk Usgae Alert" "kirankumark906@gmail.com"
     fi
   fi  
 done <<< "$disk_usage_list"
