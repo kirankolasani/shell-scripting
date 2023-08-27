ae7946c41adf2c1c3e4369d6b0f816a6c2a665e2
kirankolasani <kirankumark906@gmail.com>
2023-08-27 14:42:46 +0530

    practice

--- delete-oldfiles.sh
+++ delete-oldfiles.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
+# while IFS= read -r line
+# do
+#  echo "Reading Line : $line"
+# done < "/etc/passwd"
+
+Logdir="`pwd`/../logs"
+Date=$(date +%F-%H)
+log="$Logdir/$0-$Date.log"
+
+old_log_files=$(find Logdir -name "*" -type f -mtime +30)
+echo "$old_log_files"
+
 while IFS= read -r line
-do
- echo "Reading Line : $line"
-done < "/etc/passwd"
+do 
+  echo "Deleting $line"
+  rm -rf $line
+done < "$old_log_files"  
