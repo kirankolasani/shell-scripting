26575abbffc2a7ef780090e5a2294096612533be
kirankolasani <kirankumark906@gmail.com>
2023-08-27 15:48:12 +0530

    practice

--- disk-usage.sh
+++ disk-usage.sh
@@ -5,11 +5,14 @@ threashold_value="10%"
 while IFS= read -r line
 do 
 #   echo "Reading line : $line"
-  usage=$(echo $line|grep -vE 'Use%'|awk '{print $6}')
-  if [ "${usage%\%}" -ge "${threashold_value%\%}" ]
-  then
+  usage=$(echo "$line" | grep -vE 'Use%' | awk '{print $6}')
+  if [ -n "$usage" ]; then
+    usage_percentage="${usage%\%}"  # Remove '%' from the usage value
+    if [ "$usage_percentage" -ge "${threashold_value%\%}" ]
+    then
       echo "$usage is greather $threashold_value"
       echo "$(df -hT|grep "Use%")"
       echo "$line"
-  fi
+    fi
+  fi  
 done <<< "$disk_usage_list"
