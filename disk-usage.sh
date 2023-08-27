6f19d342aef083001009a2b87b654d95b02abe06
kirankolasani <kirankumark906@gmail.com>
2023-08-27 15:27:39 +0530

    practice

--- disk-usage.sh
+++ disk-usage.sh
@@ -6,11 +6,9 @@ while IFS= read -r line
 do 
   echo "Reading line : $line"
   usage=$(echo $line|grep -vE 'Use%'|awk '{print $6}')
-  if [ $usage => $threashold_value ]
+  if [ "$usage" -ge "$threashold_value" ]
   then
       echo "$usage is greather $threashold_value"
       echo "$line"
-  else
-      echo "IT's Okay"
   fi
 done <<< "$disk_usage_list"
