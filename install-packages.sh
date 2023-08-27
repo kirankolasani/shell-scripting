199a3c867c8815a85ba603d89611fb604cafa1b1
kirankolasani <kirankumark906@gmail.com>
2023-08-27 14:13:25 +0530

    practice

--- install-packages.sh
+++ install-packages.sh
@@ -1,44 +1,46 @@
 #!/bin/bash
 
 # This is used to install packeges 
-
+Logdir="`pwd`/../logs"
+Date="$(date +%F-%H-%M-%S)"
+logfile="$Logdir/$0-$Date.log"
 packages=("$@")
 R="\e[31m"
 G="\e[32m"
 N="\e[0m"
 Y="\e[33m"
-
+echo -e "$R $Date $N" &>>$logfile
 validate() {
     if [ $1 == 0 ];then
-     echo -e "$G $2 $N"
+     echo -e "$G $2 $N" &>>$logfile
     else 
-     echo -e "$R $3 $N" 
+     echo -e "$R $3 $N" &>>$logfile
     fi
 }
 #if [ -z "$packages" ]
 if [ ${#packages[@]} -eq 0 ]
 then 
-   echo -e " $R Please provide package names correctly to be installed $N"
+   echo -e " $R Please provide package names correctly to be installed $N" &>>$logfile
    exit 
 fi
-echo "${packages[@]}"
+echo -e "$Y entered packages ${packages[@]} $N" &>>$logfile
 for package in ${packages[@]}
 do 
-    echo "inside 1st for loop package : $package"
+   
     P=($(yum list installed|grep -wo "$package"))
     echo "${P[@]}"
     # if [ -z ${P[@]} ]
     if [ ${#P[@]} -eq 0 ]
     then
-        sudo yum install $package -y
-        validate $? "$package is installed successfully" "$package is not installed __Failed"
+        sudo yum install $package -y &>>$logfile
+        validate $? "$package is installed successfully" "$package is not installed __Failed" &>>$logfile
     else
         for name in "${P[@]}"
         do
-           echo " inside for loop name : $name"
+           
            if [ $name == $package ]
            then
-                echo -e "$Y $package is already installed $N"
+                echo -e "$Y $package is already installed $N" &>>$logfile
                 break
            # else
            #     echo ""
