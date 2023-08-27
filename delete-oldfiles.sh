bc4c974852c77f4d9c03b4e78f6de693ef837840
kirankolasani <kirankumark906@gmail.com>
2023-08-27 14:52:13 +0530

    practice

--- delete-oldfiles.sh
+++ delete-oldfiles.sh
@@ -9,11 +9,11 @@ Logdir="`pwd`/../logs"
 Date=$(date +%F-%H)
 log="$Logdir/$0-$Date.log"
 echo "$Date --started script" &>>$log
-old_log_files=$(find $Logdir -name "*" -type f -mtime +30)
+old_log_files=$(find "$Logdir" -name "*" -type f -mtime +30)
 echo "$old_log_files" &>>$log
 
 while IFS= read -r line
 do 
   echo "Deleting $line" &>>$log
-  rm -rf $line &>>$log
+  rm -rf "$line" &>>$log
 done < "$old_log_files"  
