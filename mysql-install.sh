8a6b784c6555bd3a39e48fd67174d05abd41c8a9
kirankolasani <kirankumark906@gmail.com>
2023-08-27 10:26:03 +0530

    practice

--- mysql-install.sh
+++ mysql-install.sh
@@ -2,6 +2,14 @@
 
 # This script is to install mysql 
 
-yum install mysql -y
+user=$(id -u)
+if [ $user == 0 ]
+then 
+   echo "this script is running under the user `id`"
+   yum insatll mysql -y
+else
+  echo "This mysql installation can't be run with normal user, so running with root user"
+  sudo yum install mysql -y
+fi  
 
 echo "mysql is installed successfully"
